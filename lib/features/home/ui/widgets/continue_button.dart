89f82b61176b13b5e4f79ff0e628d1e382cd192e
abdelrahmanabdelwahab <rain.forest5023@gmail.com>
2026-03-11 15:33:47 +0200

    add size and padding for the more details button

--- lib/features/home/ui/widgets/continue_button.dart
+++ lib/features/home/ui/widgets/continue_button.dart
@@ -6,8 +6,9 @@ import '../../../../core/config/double_extensions.dart';
 import '../../../../core/constants/app_radius.dart';
 import '../../../../core/constants/app_spacing.dart';
 
-class ContinueButton extends StatelessWidget{
+class ContinueButton extends StatelessWidget {
   final VoidCallback onPressed;
+
   const ContinueButton({super.key, required this.onPressed});
 
   @override
@@ -15,6 +16,7 @@ class ContinueButton extends StatelessWidget{
     return ElevatedButton(
       clipBehavior: Clip.antiAlias,
       style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(AppSpacing.xs),
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.white,
         side: BorderSide(color: Colors.white),
@@ -28,7 +30,7 @@ class ContinueButton extends StatelessWidget{
         children: [
           Text('More details', style: AppTextStyles.button),
           AppSpacing.xxs.width,
-          Icon(Icons.arrow_forward),
+          Icon(Icons.arrow_forward, size: AppSpacing.md,),
         ],
       ),
     );
