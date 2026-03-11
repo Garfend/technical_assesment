9547b28885ca4f006d829662b6c841dcb0cd4ef1
abdelrahmanabdelwahab <rain.forest5023@gmail.com>
2026-03-11 20:21:24 +0200

    remove unused imports

--- lib/features/home/ui/widgets/orders_images.dart
+++ lib/features/home/ui/widgets/orders_images.dart
@@ -2,11 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:technical_assesment/features/home/ui/widgets/total_orders_images.dart';
 
-import '../../../../app/theme/app_colors.dart';
-import '../../../../core/config/double_extensions.dart';
 import '../../../../core/config/widget_extensions.dart';
-import '../../../../core/constants/app_radius.dart';
-import '../../../../core/constants/app_spacing.dart';
 
 class OrdersImages extends StatelessWidget {
   final List<String> venueImages;
