159ee54c788d71ab4194efab20706796803e8c1e
abdelrahmanabdelwahab <rain.forest5023@gmail.com>
2026-03-11 15:08:23 +0200

    create separate component for continue button

---
+++ lib/features/home/ui/widgets/continue_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../app/theme/app_text_styles.dart';
+import '../../../../core/config/double_extensions.dart';
+import '../../../../core/constants/app_radius.dart';
+import '../../../../core/constants/app_spacing.dart';
+
+class ContinueButton extends StatelessWidget{
+  final VoidCallback onPressed;
+  const ContinueButton({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      clipBehavior: Clip.antiAlias,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.white,
+        side: BorderSide(color: Colors.white),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppRadius.circular),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('More details', style: AppTextStyles.button),
+          AppSpacing.xxs.width,
+          Icon(Icons.arrow_forward),
+        ],
+      ),
+    );
+  }
+}
